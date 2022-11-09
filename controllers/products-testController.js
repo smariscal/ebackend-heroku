20fb962d639817d6ccf5aa5ee2481c691d3c1d86
Samuel Mariscal <samuelmariscal66@gmail.com>
2022-11-08 21:20:01 -0300

    delete faker

--- controllers/products-testController.js
+++ controllers/products-testController.js
@@ -1,14 +1,12 @@
-const {faker}= require('@faker-js/faker');
-
 const generateProducts = async(req, res) =>{
   try{
     let prods=[];
     for (let i=0; i<5;i++){
       let producto={
           id:i+1,
-          productname:faker.commerce.productName(),
-          productprice:faker.commerce.price(150,2000,0,'$'),
-          productthumbnail:faker.image.business(150, 150, true)
+          productname:i,
+          productprice:i*100,
+          productthumbnail:"https://picsum.photos/100"
       }
       prods.push(producto);
     }
